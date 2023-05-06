0ed898be2cd7f20e3a478c458839318066ec46a5
sindi.lil18 <sindi.lil18@gmail.com>
2023-05-06 11:26:59 +0200

    Add Announcement

--- Dormitory/Program.cs
+++ Dormitory/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace Dormitory;
+﻿using Dormitory.DAL;
+
+namespace Dormitory;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World from Sindi!");
+        //using dormitorycontext
+        //te dhenat merren nga perdoruesi
+
+        using var context = new DormitoryContext();
+
+        Console.WriteLine("Enter announcement title: ");
+        var announcementTitle = Console.ReadLine();
+
+        Console.WriteLine("Enter announcement description: ");
+        var announcementDescription = Console.ReadLine();
+
+        //vejme _ sepse sna duhet response qe na kthen ky kod thjesht 
+        //
+        _ = context.Announcements.Add(new Announcement
+        {
+            Title = announcementTitle,
+            Description = announcementDescription,
+            PublishedDate = DateTime.Now,
+            IsActive = true
+        });
+
+        _ = context.SaveChanges();
+
     }
+
 }
